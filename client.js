88e9893625d6cad145884fab1afd739afd5bb2a0
mauroapjr <mauroapjr@outlook.com>
2023-01-18 18:46:47 -0500

    finished challenge

--- client.js
+++ client.js
@@ -1,11 +1,22 @@
+const { Server } = require("http");
 const net = require("net");
+const server = net.createServer();
+
 const connect = function () {
   const conn = net.createConnection({
     host: "172.29.32.215",
-    port: 50541
+    port: 50541,
   });
 
-  // interpret incoming data as text
+  conn.on("connect", function () {
+    console.log("Welcome to the game!");
+    conn.write("Name: MAP");
+  });
+
+  // process.stdin.on('Name: SNK', function(message){
+  //   conn.write("Name: ___");
+  // });
+
   conn.setEncoding("utf8");
 
   return conn;
@@ -13,5 +24,4 @@ const connect = function () {
 
 console.log("Connecting ...");
 
-
-module.exports = connect
+module.exports = connect;
