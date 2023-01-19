15fdff467367238c4dbcaa200d5625b4c325c4e4
mauroapjr <mauroapjr@outlook.com>
2023-01-18 23:09:55 -0500

    finished challenge

--- client.js
+++ client.js
@@ -25,4 +25,4 @@ const connect = function () {
   
 };
 
-module.exports = connect;
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,37 +1,43 @@
-const net = require("net");
-const connect = require("./client");
-const connection = connect();
+//const net = require("net");
+//const connect = require("./client");
+//const connection = connect();
+//const setupFromInput = setupInput();
+//const { handleUserFromInput = handleUserInput();
 
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    
-    if (key === "\u0003") process.exit();
-    if (key === "w") {
-      connection.write("Move: up")
-    }
-    if (key === "s") {
-      connection.write("Move: down")
-    }
-    if (key === "a") {
-      connection.write("Move: left")
-    }
-    if (key === "d") {
-      connection.write("Move: right")
-    }
-    if (key === "y") connection.write("Say: Sup!");
-    if (key === "u") connection.write("Say: War!");
-    if (key === "i") connection.write("Say: Battle?");
-    prevKey = key;
-  };
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
+connect();
+
 setupInput();
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+
+//   const handleUserInput = function (key) {
+
+//     if (key === "\u0003") process.exit();
+//     if (key === "w") {
+//       connection.write("Move: up")
+//     }
+//     if (key === "s") {
+//       connection.write("Move: down")
+//     }
+//     if (key === "a") {
+//       connection.write("Move: left")
+//     }
+//     if (key === "d") {
+//       connection.write("Move: right")
+//     }
+//     if (key === "y") connection.write("Say: Sup!");
+//     if (key === "u") connection.write("Say: War!");
+//     if (key === "i") connection.write("Say: Battle?");
+//     prevKey = key;
+//   };
+//   stdin.on("data", handleUserInput);
+//   return stdin;
+// };
