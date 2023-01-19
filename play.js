b948c1c9f983a13d3e1be476c8711a5313f76b45
mauroapjr <mauroapjr@outlook.com>
2023-01-18 23:11:34 -0500

    finished challenge

--- play.js
+++ play.js
@@ -1,8 +1,3 @@
-//const net = require("net");
-//const connect = require("./client");
-//const connection = connect();
-//const setupFromInput = setupInput();
-//const { handleUserFromInput = handleUserInput();
 
 const { connect } = require("./client");
 const { setupInput } = require("./input");
@@ -12,32 +7,3 @@ connect();
 
 setupInput();
 
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-
-//   const handleUserInput = function (key) {
-
-//     if (key === "\u0003") process.exit();
-//     if (key === "w") {
-//       connection.write("Move: up")
-//     }
-//     if (key === "s") {
-//       connection.write("Move: down")
-//     }
-//     if (key === "a") {
-//       connection.write("Move: left")
-//     }
-//     if (key === "d") {
-//       connection.write("Move: right")
-//     }
-//     if (key === "y") connection.write("Say: Sup!");
-//     if (key === "u") connection.write("Say: War!");
-//     if (key === "i") connection.write("Say: Battle?");
-//     prevKey = key;
-//   };
-//   stdin.on("data", handleUserInput);
-//   return stdin;
-// };
